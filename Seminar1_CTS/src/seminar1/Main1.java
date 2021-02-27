6ea1acabe58ff266b4821230f363c47d27d003fa
bianc <radubianca18@stud.ase.ro>
2021-02-27 11:21:47 +0200

    Am adaugat clasa WildCat!

--- Seminar1_CTS/src/seminar1/Main1.java
+++ Seminar1_CTS/src/seminar1/Main1.java
@@ -2,6 +2,7 @@ package seminar1;
 
 import clase.Animal;
 import clase.Lion;
+import clase.WildCat;
 import clase.Zoo;
 
 public class Main1 {
@@ -13,6 +14,14 @@ public class Main1 {
 		Lion lion2=new Lion("Leona");
 		zoo.addAnimal(lion1);
 		zoo.addAnimal(lion2);
+		zoo.feedAllAnimals();
+		
+		WildCat cat=new WildCat("Pisica");
+		WildCat cat2=new WildCat("Pisica2", "mancare2");
+		
+		
+		zoo.addAnimal(cat);
+		zoo.addAnimal(cat2);
 		zoo.feedAllAnimals();
 	}
 
