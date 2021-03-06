9c817f5267049c93fe7cbfb446cba6bf8692bb6f
bianc <radubianca18@stud.ase.ro>
2021-03-06 10:50:08 +0200

    Modificare clase si creare interfata!

--- Seminar2_cts/src/ro/ase/cts/program/Program.java
+++ Seminar2_cts/src/ro/ase/cts/program/Program.java
@@ -3,17 +3,22 @@ package ro.ase.cts.program;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.reader.ReaderAngajati;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.reader.ReaderAplicant;
+import ro.ase.cts.reader.ReaderStudent;
 
 public class Program {
+	
+	public static List<Aplicant> citireAplicanti(String fisier, ReaderAplicant reader) throws FileNotFoundException {
+		return reader.readAplicanti(fisier);
+	}
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> lista;
 		try {
-			listaAngajati = ReaderAngajati.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			lista = citireAplicanti("elevi.txt",new ReaderStudent());
+			for(Aplicant a:lista)
+				System.out.println(a.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- Seminar2_cts/src/ro/ase/cts/reader/ReaderAngajati.java
+++ Seminar2_cts/src/ro/ase/cts/reader/ReaderAngajati.java
@@ -7,37 +7,13 @@ import java.util.List;
 import java.util.Scanner;
 
 import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Elev;
-import ro.ase.cts.clase.Student;
+import ro.ase.cts.clase.Aplicant;
 
-public class ReaderAngajati {
-	public static List<Student> readStudents(String file) throws FileNotFoundException, NumberFormatException {
-		Scanner input = new Scanner(new File(file));
-		input.useDelimiter(",|\n");
-		List<Student> studenti = new ArrayList<Student>();
-
-		while (input.hasNext()) {
-			String nume = input.next();
-			String prenume = (input.next()).toString();
-			int varsta = Integer.valueOf(input.nextInt());
-			int punctaj = Integer.valueOf(input.nextInt());
-			int nr = Integer.valueOf(input.nextInt());
-			String[] vect = new String[5];
-			for (int i = 0; i < nr; i++)
-				vect[i] = input.next();
-			int an_studii = input.nextInt();
-			String facultate = (input.next()).toString();
-			Student s = new Student(nume, prenume, varsta, punctaj, nr, vect, facultate, an_studii);
-			studenti.add(s);
-		}
-		input.close();
-		return studenti;
-	}
-
-	public static List<Angajat> readAngajati(String file) throws FileNotFoundException {
+public class ReaderAngajati implements ReaderAplicant {
+	public List<Aplicant> readAplicanti(String file) throws FileNotFoundException {
 		Scanner input2 = new Scanner(new File(file));
 		input2.useDelimiter(",");
-		List<Angajat> angajati = new ArrayList<Angajat>();
+		List<Aplicant> angajati = new ArrayList<Aplicant>();
 
 		while (input2.hasNext()) {
 			String nume = input2.next();
@@ -56,32 +32,6 @@ public class ReaderAngajati {
 		input2.close();
 		return angajati;
 	}
-
-	public static List<Elev> readPupil(String file) throws FileNotFoundException {
-		Scanner input2 = new Scanner(new File(file));
-		input2.useDelimiter(",|\n");
-		List<Elev> elevi = new ArrayList<Elev>();
-
-		while (input2.hasNext()) {
-			String nume = input2.next();
-			String prenume = input2.next();
-			int varsta = input2.nextInt();
-			int punctaj = input2.nextInt();
-			int nr = input2.nextInt();
-			String[] vect = new String[5];
-			for (int i = 0; i < nr; i++)
-				vect[i] = input2.next();
-			int clasa = input2.nextInt();
-			String tutore = input2.next();
-			Elev e = new Elev(nume, prenume, varsta, punctaj, nr, vect, clasa, tutore);
-			elevi.add(e);
-		}
-
-		input2.close();
-		return elevi;
-	}
-
-	
 
 
 }
