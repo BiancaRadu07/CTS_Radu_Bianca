6753a8e27f2dbadff10fdff9c05f4234995247c6
bianc <radubianca18@stud.ase.ro>
2021-05-22 11:20:11 +0300

    JUnit part 2

--- TestStudent/src/tests/StudentsTests.java
+++ TestStudent/src/tests/StudentsTests.java
@@ -37,4 +37,62 @@ public class StudentsTests {
 		student.adaugaNota(nota);
 		assertEquals(10, student.getNota(0));
 	}
+	
+	@Test
+	public void dimensiuneLista() {
+		Student student = new Student();
+		student.adaugaNota(10);
+		student.adaugaNota(9);
+		assertEquals(2, student.getNote().size());
+		
+	}
+	@Test
+	public void calculeazaMediaCorect() {
+		Student student = new Student();
+		student.adaugaNota(10);
+		student.adaugaNota(9);
+		assertEquals(9.5f, student.calculeazaMedie(), 0.01);
+	}
+	@Test
+	public void testStudentCuRestanta() {
+		Student student = new Student();
+		student.adaugaNota(4);
+		assertTrue(student.areRestante());
+	}
+	
+	@Test
+	public void testStudentFaraRestanta() {
+		Student student = new Student();
+		student.adaugaNota(10);
+		assertFalse(student.areRestante());
+	}
+	
+	@Test
+	public void testDeNotaIndexInvalid() {
+		Student student = new Student();
+		student.adaugaNota(10);
+		//1
+		try {
+			//2
+			int nota = student.getNota(-1);
+			//3 nu se ajunge
+			fail("Nu trebuia sa ajunga aici. Metoda nu arunca exceptie");
+		}
+		catch(IndexOutOfBoundsException exception){
+			//4
+		}
+		catch(Exception exception) {
+			//5 nu se ajunge
+			fail("Tipul exceptiei aruncate nu este corect");
+		}
+		//6
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetNotaIndexInvalidJUnit4() {
+		Student student = new Student();
+		student.adaugaNota(10);
+		
+		int nota = student.getNota(-1);
+	}
 }
